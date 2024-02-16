28b88db2305ce41826b95ee3017b63b7b9255a4a
jlopeza2020 <j.lopeza.2020@alumnos.urjc.es>
2024-02-16 14:50:09 +0100

    added eje1

---
+++ ejercicios/ejercicio1/argsort.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Punteros a string
+void
+swapstrings(char **str1, char **str2)
+{
+	char *temp = *str1;
+
+	*str1 = *str2;
+	*str2 = temp;
+}
+
+void
+sortargs(char **strings, int n)
+{
+	int i;
+	int j;
+
+	for (i = 1; i < n; i++) {
+		for (j = 0; j < i; j++) {
+			/*Intercambiar valores cuando s1 > s2 así,
+			   el orden tiene que ir de menor a mayor 
+			   debido a la tabla ASCII */
+			if (strcmp(strings[j], strings[i]) > 0) {
+				swapstrings(&strings[j], &strings[i]);
+			}
+		}
+	}
+}
+
+void
+printforwardargs(char **strings, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("%s\n", strings[i]);
+	}
+}
+
+void
+printbackwardargs(char **strings, int n)
+{
+	int i;
+
+	for (i = n-1; i >= 0; --i) {
+		printf("%s\n", strings[i]);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	char *varenv;
+    char *forward;
+    char *backward;
+    /*Omitimos el primer argumento: 
+	   es la invocación al propio programa */
+	argc--;
+	argv++;
+
+    forward = "forward";
+    backward = "backward";
+
+	sortargs(argv, argc);
+
+    varenv = getenv("DIRECTION");
+    /*if (varenv == NULL){ 
+        printf("soy NULL\n");
+    }*/
+    fprintf(stderr,"%s\n", varenv); // eliminar
+
+    if(strcmp(varenv, forward) == 0 || varenv == NULL) // CAMBIAR ORDEN 
+    {
+        printforwardargs(argv, argc);
+
+    }
+
+    if(strcmp(varenv, backward) == 0)
+    {
+        printbackwardargs(argv, argc);
+
+    }
+	//printforwardargs(argv, argc);
+    //printbackwardargs(argv, argc);
+
+    // si está definida o no es forward o backward -> dar un error -> usage
+
+	exit(EXIT_SUCCESS);
+}
