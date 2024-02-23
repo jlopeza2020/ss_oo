6c9e1114a37b8c34ec5ed2096dccdce52f5dee44
jlopeza2020 <j.lopeza.2020@alumnos.urjc.es>
2024-02-23 12:32:16 +0100

    final version p2

--- ejercicios/ejercicio2/pinger.c
+++ ejercicios/ejercicio2/pinger.c
@@ -6,74 +6,61 @@
 #include <sys/wait.h>
 enum {
 
-	NoArgs,		
+	NoArgs,
 };
 
 void
 usage(void)
 {
-	fprintf(stderr,
-		"usage: pinger ip/name [ip/name ...]\n");
+	fprintf(stderr, "usage: pinger ip/name [ip/name ...]\n");
 	exit(EXIT_FAILURE);
 }
-
 
 int
 main(int argc, char *argv[])
 {
 	int pid;
-    int sts;
-    int i; 
-    int exitsts;
-    
-    // Omitimos primer argumento
+	int sts;
+	int i;
+	int exitsts;
+
+	// Omitimos primer argumento
 	argc--;
 	argv++;
 
-    if(argc == NoArgs){
-        usage();
-    }
+	if (argc == NoArgs) {
+		usage();
+	}
 
-    for(i = 0; i < argc; i++){
+	for (i = 0; i < argc; i++) {
 
-        pid = fork();
+		pid = fork();
 
-        switch(pid){
-            case -1:
-                err(EXIT_FAILURE, "fork failed!");
-            case 0:
-                execl("/bin/ping", "ping", "-c", "1", "-w", "5", argv[i], NULL);
-                err(EXIT_FAILURE , "exec failed");
-            default:
-                printf("child created : %d\n", pid );
-        }
-    }
+		switch (pid) {
+		case -1:
+			err(EXIT_FAILURE, "fork failed!");
+		case 0:
+			execl("/bin/ping", "ping", "-c", "1", "-w", "5",
+			      argv[i], NULL);
+			err(EXIT_FAILURE, "exec failed");
+		default:
+			//printf("child created : %d\n", pid );
+		}
+	}
 
-    exitsts = 0;
-    while((pid = wait(&sts)) != -1){ 
+	exitsts = 0;
+	while ((pid = wait(&sts)) != -1) {
 
-        if(WIFEXITED(sts)) {
-            if(WEXITSTATUS(sts) != 0){
-                exitsts = 1;
-            }
-        }
-        //printf("Did process %d exit?\n", pid);
+		if (WIFEXITED(sts)) {
+			if (WEXITSTATUS(sts) != 0) {
+				exitsts = 1;
+			}
+		}
+	}
 
-        /*if(WIFEXITED(sts)) {
-            printf("Yes! the status was : %d\n" , WEXITSTATUS(sts));
+	if (exitsts == 1) {
+		exit(EXIT_FAILURE);
+	}
 
-        } else {
-            printf("No\n");
-        }*/
-    }
-
-    if(exitsts == 1){
-        exit(EXIT_FAILURE);
-    }
-
-    // comprobar si alguno de ellos ha fallado
-    // crear un array de la dimensión del número de argumentos e ir almacenando 
-    // su valor dentro de él 
-    fprintf(stderr, "values exists %d\n", exitsts);
 	exit(EXIT_SUCCESS);
 }
