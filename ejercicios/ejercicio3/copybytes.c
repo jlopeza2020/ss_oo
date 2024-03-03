a37349f72225e009e702e4c6f76cecd7a7f5818f
Julia <jlopeza2020@alumnos.urjc.es>
2024-03-03 13:51:36 +0100

    copybytes added is number arg

--- ejercicios/ejercicio3/copybytes.c
+++ ejercicios/ejercicio3/copybytes.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 enum{
     EnoughArgs = 3,
@@ -15,17 +16,48 @@ usage(){
 
 }
 
-int
-isnumber(){
+long
+isnumber(char *str){
+
+    int base;
+    char *endptr;
+    long val;
+    base = 10;
+
+    // Para distinguir entre success/failure después de la llamada
+    errno = 0;
+    val = strtol(str, &endptr, base);
+        
+    //strtol(str, &endptr, base);
 
 
+    // Se comprueban posibles errores 
+    if (errno != 0) {
+        perror("strtol");
+        exit(EXIT_FAILURE);
+    }
+
+    if (endptr == str) {
+        fprintf(stderr, "No digits were found\n");
+        exit(EXIT_FAILURE);
+    }
+    // Si llegamos hasta aquí strtol() ha sido capaz de parsear un número
+    // Ahora es necesario comprobar si la string ha sido un número o no
+
+    if (*endptr != '\0'){    
+        // Devuelve fallo
+        //return 0;
+        fprintf(stderr, "Is not a complete number\n");
+        exit(EXIT_FAILURE);
+    }
+    // Devuelve éxito
+    return val;
 }
 
 int
 main(int argc, char *argv[]){
 
-    //char *buffer; 
-    //char *numbytes;
+    long buffsize;
     
     argc--; 
     argv++;
@@ -33,18 +65,28 @@ main(int argc, char *argv[]){
     switch(argc){
 
     case EnoughArgs:
-            // trata solo lo de 3 argumentos 
-        //printf("hola 3 \n");
-        if (isnumber())
+        // se comprueba que el tamaño del buffer sea un número
+        buffsize = isnumber(argv[2]);
+        printf("es un numero: %ld\n", buffsize);
+
         break;
     case MaxArgs:
-            // trata 4 argumentos
-        //printf("hola 4 \n");
+        // se comprueba que el tamaño del buffer sea un número
+        buffsize = isnumber(argv[2]);
+        printf("es un numero: %ld\n", buffsize);
+
+        
         break;
 
     default:
         usage();
     }
+
+    printf("hola\n");
+
+    // comprueba el fichero origen 
+    // destino 
+    // se opera 
 
 
     /*
