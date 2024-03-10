09bac61504f62d8f2ff790ee7a017e2d498e3faa
Julia <jlopeza2020@alumnos.urjc.es>
2024-03-10 21:16:16 +0100

    done indented version

--- ejercicios/ejercicio4/ngrams.c
+++ ejercicios/ejercicio4/ngrams.c
@@ -7,28 +7,26 @@
 #include <unistd.h>
 #include <string.h>
 
-
 enum {
 	SourceFile,
 	DestinationFile,
 	MaxArgs,
 };
 
-struct Circulararray{ 
+struct Circulararray {
 
-    char *array;
-    int front;
-    int end;
-    long size;
-    long szcounter;
+	char *array;
+	int front;
+	int end;
+	long size;
+	long szcounter;
 };
-typedef struct Circulararray Circulararray; 
+typedef struct Circulararray Circulararray;
 
 void
 usage()
 {
-	fprintf(stderr,
-		"usage: ./ngrams routefile ngramsize\n");
+	fprintf(stderr, "usage: ./ngrams routefile ngramsize\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -82,7 +80,7 @@ getfd(char *path, int tipefile)
 		}
 		// usar open en modo lectura 
 		//fd = open(path, O_RDONLY);
-        fd = fopen(path, "r");
+		fd = fopen(path, "r");
 
 		if (fd == NULL) {
 			err(EXIT_FAILURE, "fopen");
@@ -93,7 +91,7 @@ getfd(char *path, int tipefile)
 		// con los permisos escritos en el tercer parámetro 
 		//fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, mode);
 		fd = fopen(path, "w");
-        if (fd == NULL) {
+		if (fd == NULL) {
 			err(EXIT_FAILURE, "fopen");
 		}
 	}
@@ -101,59 +99,59 @@ getfd(char *path, int tipefile)
 	return fd;
 }
 
-int 
-isfull(Circulararray *q){
+int
+isfull(Circulararray *q)
+{
 	return (((q->end + 1) % q->size) == q->front);
 }
 
 int
-isempty(Circulararray *q){
+isempty(Circulararray *q)
+{
 	return (q->front == -1 && q->end == -1);
 }
 
-// en un futuro se encolarán chars no ints
 void
 enqueue(Circulararray *q, int x)
 {
-    // si el array está vacío ambos punteros se sitúan 
+	// si el array está vacío ambos punteros se sitúan 
 	// en la posición 0 y se almacena en la posición 
 	// del puntero "end" (es 0), el valor de x.
-	if(q->front == -1 && q->end == -1){
-        q->front = q->end = 0;
-        q->array[q->end] = x;
+	if (q->front == -1 && q->end == -1) {
+		q->front = q->end = 0;
+		q->array[q->end] = x;
 
-    }else if (isfull(q)){ 
-        printf("circular array is full\n");
-        return;
-    }else{
+	} else if (isfull(q)) {
+		printf("circular array is full\n");
+		return;
+	} else {
 		// si hay algún elemento en el array: 
 		// se incrementa la posición de end 
 		// y se almacena en ella el valor de x.
-        q->end = (q->end + 1) % q->size;
-        q->array[q->end] = x;
-    }
-    q->szcounter++;
+		q->end = (q->end + 1) % q->size;
+		q->array[q->end] = x;
+	}
+	q->szcounter++;
 }
 
 void
 dequeue(Circulararray *q)
-{   
-	if(isempty(q)){
-        printf("circular array is empty\n");
-        return;
-    }
+{
+	if (isempty(q)) {
+		printf("circular array is empty\n");
+		return;
+	}
 	// cuando front y end se encuentran en la misma posición: 
 	// (solo queda un elemento en el array)
 	// se fijan a -1 para indicar que está vacío el array
-    if (q->front == q->end){
-       q->front = q->end = -1; 
-    }
-    else{
+	if (q->front == q->end) {
+		q->front = q->end = -1;
+	} else {
 		// si hay más de un elemento en el array: 
 		// se avanza una posición del puntero "front"
-        q->front = (q->front + 1) % q->size;
-    }
-    q->szcounter--;
+		q->front = (q->front + 1) % q->size;
+	}
+	q->szcounter--;
 
 }
 
@@ -161,89 +159,81 @@ dequeue(Circulararray *q)
 void
 display(Circulararray *q)
 {
-    int i = q->front;
-    
-	if(isempty(q)){
-        printf("circular array is empty\n");
-        return;
-    }else{
-        printf("[");
-        while( i != q->end ){
-            printf("%c", q->array[i]);
-            i = (i+1) % q->size;
-        }
-    }
-    printf("%c", q->array[q->end]);
-    printf("]\n");
+	int i = q->front;
+
+	if (isempty(q)) {
+		printf("circular array is empty\n");
+		return;
+	} else {
+		printf("[");
+		while (i != q->end) {
+			printf("%c", q->array[i]);
+			i = (i + 1) % q->size;
+		}
+	}
+	printf("%c", q->array[q->end]);
+	printf("]\n");
 
 }
 
+void
+printgrams(char *srcpath, long size)
+{
 
-void 
-printgrams(char *srcpath, long size){
+	FILE *srcfd;
+	Circulararray ca;
+	char c;
 
-    FILE *srcfd;
-    Circulararray ca;
-    //size_t bytesread;
-    //size_t i;
-    char c;
+	//inicializada la estructura de datos
+	ca.size = size;
+	ca.array = (char *)malloc(sizeof(char) * ca.size);
+	ca.front = -1;
+	ca.end = -1;
+	ca.szcounter = 0;
 
-    //inicializada la estructura de datos
-    ca.size = size;
-    ca.array = (char *)malloc(sizeof(char)* ca.size);  
-    ca.front = -1;
-    ca.end = -1;
-    ca.szcounter = 0;
+	srcfd = getfd(srcpath, SourceFile);
 
-    srcfd = getfd(srcpath, SourceFile);
+	// lee hasta final de fichero
+	while (fread(&c, sizeof(char), 1, srcfd) != 0) {
 
-    // feof: devuelve un número distinto de 0 si ha llegado a final del fichero
-    //while(!feof(srcfd)){
-    while (fread(&c, sizeof(char), 1, srcfd) != 0) {
+		enqueue(&ca, c);
+		if (ca.szcounter == size) {
+			display(&ca);
+			dequeue(&ca);
+		}
+		// ferror: devuelve un número distinto de 0 si hay error. 
+		if (ferror(srcfd)) {
+			errx(EXIT_FAILURE, "fread error ocurred\n");
+		}
+	}
 
-        //bytesread = fread(&c,1, ca.size, srcfd);
-        //for(i = 0; i < bytesread; i++){
-            // añade y se hace display
-            //fprintf(stderr, "len  %ld ca.array\n", i);
-        enqueue(&ca, c);
-        if(ca.szcounter == size){ // si la dimensión es 3 imprimirlo
-            display(&ca);
-            dequeue(&ca);
-        }
-        //}
-
-        // ferror: devuelve un número distinto de 0 si está establecido. 
-        if (ferror(srcfd)) {
-            errx(EXIT_FAILURE , "fread error ocurred\n");
-        }
-    }
-    
-    fclose(srcfd);
-    free(ca.array);
+	fclose(srcfd);
+	free(ca.array);
 }
 
 int
-main(int argc, char *argv[]){
+main(int argc, char *argv[])
+{
 
-    char *srcpath;
-    char *buffersize;
-    long ngramsize;
+	char *srcpath;
+	char *buffersize;
+	long ngramsize;
 
-    argc--;
+	argc--;
 	argv++;
-    
-    if(argc != MaxArgs){
-        usage();
-    }
-    srcpath = argv[0];
 
-    buffersize = argv[1];
-    ngramsize = getnumber(buffersize);
-    if (ngramsize <= 0) {
+	if (argc != MaxArgs) {
+		usage();
+	}
+	srcpath = argv[0];
+
+	buffersize = argv[1];
+	ngramsize = getnumber(buffersize);
+	if (ngramsize <= 0) {
 		errx(EXIT_FAILURE, "second parameter should be bigger than 0");
 	}
 
-    printgrams(srcpath, ngramsize);
-    
-    exit(EXIT_SUCCESS);
+	printgrams(srcpath, ngramsize);
+
+	exit(EXIT_SUCCESS);
 }
