8c61ba73bef88df01144d91b8414234c4661fed4
Julia <jlopeza2020@alumnos.urjc.es>
2024-03-25 18:42:08 +0100

    indented working version

--- ejercicios/ejercicio5/sourcefiles.c
+++ ejercicios/ejercicio5/sourcefiles.c
@@ -11,15 +11,15 @@
 
 enum {
 	ZeroArgs,
-    MaxPath = 8*1024, // 8K
-    MaxLine = 256,
+	MaxPath = 8 * 1024,	// 8K
+	MaxLine = 256,
 };
 
-struct Sourcefiles{
-    char *path;
-    long cfiles;
-    long hfiles;
-    long long totalbytes;
+struct Sourcefiles {
+	char *path;
+	long cfiles;
+	long hfiles;
+	long long totalbytes;
 };
 typedef struct Sourcefiles Sourcefiles;
 
@@ -30,138 +30,146 @@ usage(void)
 	exit(EXIT_FAILURE);
 }
 
-char *getcompletepath(char *path, char *dname){
+char *
+getcompletepath(char *path, char *dname)
+{
 
-    char *fullpath;
-    ssize_t lenpath;
-    ssize_t lenname;
-    ssize_t lenfull;
+	char *fullpath;
+	ssize_t lenpath;
+	ssize_t lenname;
+	ssize_t lenfull;
 
-    // obtenemos la longitud del path
-    lenpath = strlen(path);
-    // longitud del nombre de entrada de directorio 
-    lenname = strlen(dname);
+	// obtenemos la longitud del path
+	lenpath = strlen(path);
+	// longitud del nombre de entrada de directorio 
+	lenname = strlen(dname);
 
-    // path + / + nombre de entrada directorio + \0 
-    lenfull = lenpath + lenname + 2; 
+	// path + / + nombre de entrada directorio + \0 
+	lenfull = lenpath + lenname + 2;
 
-    // comprobar que el malloc no es muy grande 
-    if (lenfull > MaxPath){
-        perror("Invalid path size");
-    }
-
-    fullpath = (char *)malloc(sizeof(char) * (lenfull));
-    if (fullpath == NULL) {
-        perror("Error: dynamic memory cannot be allocated");
+	// comprobar que el malloc no es muy grande 
+	if (lenfull > MaxPath) {
+		perror("Invalid path size");
 	}
 
-    strncpy(fullpath, path, lenfull);
-    // después del path añadir '/'
-    fullpath[lenpath] = '/';
-    // strncpy no añade \0 a final de línea
-    fullpath[lenfull - 1] = '\0';
-    strncpy(fullpath + lenpath + 1, dname, lenfull - lenpath - 1);
-    fullpath[lenfull - 1] = '\0';
-    
-    return fullpath;
+	fullpath = (char *)malloc(sizeof(char) * (lenfull));
+	if (fullpath == NULL) {
+		perror("Error: dynamic memory cannot be allocated");
+	}
+
+	strncpy(fullpath, path, lenfull);
+	// después del path añadir '/'
+	fullpath[lenpath] = '/';
+	// strncpy no añade \0 a final de línea
+	fullpath[lenfull - 1] = '\0';
+	strncpy(fullpath + lenpath + 1, dname, lenfull - lenpath - 1);
+	fullpath[lenfull - 1] = '\0';
+
+	return fullpath;
 }
 
 int
-isfile(char *name, char *extension){
+isfile(char *name, char *extension)
+{
 
-    char *value;
+	char *value;
 
-    value = strrchr(name, '.');
-    if(value != NULL){
-        if(strcmp(value, extension) == 0){
-            return 1; // exito: son iguales
-        }
-    }
-    return 0;
+	value = strrchr(name, '.');
+	if (value != NULL) {
+		if (strcmp(value, extension) == 0) {
+			return 1;	// exito: son iguales
+		}
+	}
+	return 0;
 }
 
-void 
-processdirectory(char *path, Sourcefiles *infosrc){
+void
+processdirectory(char *path, Sourcefiles *infosrc)
+{
 
-    DIR *d;
-    struct dirent *ent;
-    struct stat sb;
-    char *fullpath;
+	DIR *d;
+	struct dirent *ent;
+	struct stat sb;
+	char *fullpath;
 
-    d = opendir(path);
-    if (d == NULL) {
-        perror("opendir failed");
-    }else{
+	d = opendir(path);
+	if (d == NULL) {
+		perror("opendir failed");
+	} else {
 
-        while ((ent = readdir(d)) != NULL) {
-            if (ent->d_name[0] != '.') {
+		while ((ent = readdir(d)) != NULL) {
+			if (ent->d_name[0] != '.') {
 
-                fullpath = getcompletepath(path, ent->d_name);
+				fullpath = getcompletepath(path, ent->d_name);
 
-                // solo si es directorio o fichero convencional seguirá
-                // con el bucle. Omite los enlaces simbólicos
-                if (lstat(fullpath, &sb) < 0) {
-                    free(fullpath);
-                    perror("lstat");
-		        }
+				// solo si es directorio o fichero convencional seguirá
+				// con el bucle. Omite los enlaces simbólicos
+				if (lstat(fullpath, &sb) < 0) {
+					free(fullpath);
+					perror("lstat");
+				}
 
-		        if ((sb.st_mode & S_IFMT) == S_IFDIR) {
-                    processdirectory(fullpath, infosrc);
+				if ((sb.st_mode & S_IFMT) == S_IFDIR) {
+					processdirectory(fullpath, infosrc);
 
-		        }else if ((sb.st_mode & S_IFMT) == S_IFREG){
+				} else if ((sb.st_mode & S_IFMT) == S_IFREG) {
 
-                    if (isfile(ent->d_name, ".c")){
-                        infosrc->cfiles++;
-                        infosrc->totalbytes += sb.st_size;
-                    }
-                    if(isfile(ent->d_name, ".h")){
-                        infosrc->hfiles++;
-                        infosrc->totalbytes += sb.st_size;
-                    }
-                }
+					if (isfile(ent->d_name, ".c")) {
+						infosrc->cfiles++;
+						infosrc->totalbytes +=
+						    sb.st_size;
+					}
+					if (isfile(ent->d_name, ".h")) {
+						infosrc->hfiles++;
+						infosrc->totalbytes +=
+						    sb.st_size;
+					}
+				}
 
-                free(fullpath);
-            }
-        }
-        closedir(d);
-    }
+				free(fullpath);
+			}
+		}
+		closedir(d);
+	}
 }
 
-int 
-main(int argc, char *argv[]){
+int
+main(int argc, char *argv[])
+{
 
-    // preguntar si hace falta malloc porque si se pasa 
-    // de tamaño hace una segunda lectura el programa por si solo 
-    // preguntar tambien por el uso de errno  ¿aplicarlo también a malloc...? 
+	// preguntar si hace falta malloc porque si se pasa 
+	// de tamaño hace una segunda lectura el programa por si solo 
+	// preguntar tambien por el uso de errno  ¿aplicarlo también a malloc...? 
 
-    char line[MaxLine];
-    Sourcefiles infodir;
-    
-    errno = 0;
+	char line[MaxLine];
+	Sourcefiles infodir;
 
-    argc--;
+	errno = 0;
+
+	argc--;
 	argv++;
 
-    if (argc != ZeroArgs) {
+	if (argc != ZeroArgs) {
 		usage();
 	}
 
-    while(fgets(line, MaxLine, stdin) != NULL){
+	while (fgets(line, MaxLine, stdin) != NULL) {
 
-        // elimina el salto de línea al final
-        line[strcspn(line, "\n")] = 0;
+		// elimina el salto de línea al final
+		line[strcspn(line, "\n")] = 0;
 
-        infodir.path = line;
-        infodir.cfiles = 0;
-        infodir.hfiles = 0;
-        infodir.totalbytes = 0;
+		infodir.path = line;
+		infodir.cfiles = 0;
+		infodir.hfiles = 0;
+		infodir.totalbytes = 0;
 
-        processdirectory(line, &infodir);
-        printf("%s\t%ld\t%ld\t%lld\n", infodir.path, infodir.cfiles, infodir.hfiles, infodir.totalbytes);
-    }
-    if(!feof(stdin)){
-        errx(EXIT_FAILURE, "eof not reached\n");
-    }
+		processdirectory(line, &infodir);
+		printf("%s\t%ld\t%ld\t%lld\n", infodir.path, infodir.cfiles,
+		       infodir.hfiles, infodir.totalbytes);
+	}
+	if (!feof(stdin)) {
+		errx(EXIT_FAILURE, "eof not reached\n");
+	}
 
-    exit(errno);
+	exit(errno);
 }
