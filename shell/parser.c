7c7258502a07a33b54ba9cd7560b3ae59ce86ea2
Julia <jlopeza2020@alumnos.urjc.es>
2024-04-02 17:50:32 +0200

    indented version

--- shell/parser.c
+++ shell/parser.c
@@ -3,240 +3,246 @@
 #include <string.h>
 #include "parser.h"
 
-
 // según se fijan cosas habrá que ir mirando los descriptores de
 // fichero y demás 
 void
-settype(CheckInput *checkinput, int val){
+settype(CheckInput * checkinput, int val)
+{
 
-    // CHECKINPUT DE MOMENTO NO SE USA 
+	// CHECKINPUT DE MOMENTO NO SE USA 
 
-    switch (val) {
+	switch (val) {
 
 	case PIPE:
-        //checkinput->numpipes++;
-        fprintf(stderr, "soy pipe\n");
+		//checkinput->numpipes++;
+		fprintf(stderr, "soy pipe\n");
 
 		break;
 	case STDINRED:
-        fprintf(stderr, "soy redirección de entrada\n");
+		fprintf(stderr, "soy redirección de entrada\n");
 		break;
-    case STDOUTRED:
-        fprintf(stderr, "soy redirección de salida\n");
+	case STDOUTRED:
+		fprintf(stderr, "soy redirección de salida\n");
 		break;
 
-    case BACKGROUND:
-        fprintf(stderr, "ejecuto en background\n");
-	    break;
+	case BACKGROUND:
+		fprintf(stderr, "ejecuto en background\n");
+		break;
 
-    case ENV:
-        fprintf(stderr, "soy variable de entorno\n");
-	    break;
+	case ENV:
+		fprintf(stderr, "soy variable de entorno\n");
+		break;
 
-    case EQUAL:
-        fprintf(stderr, "soy igual\n");
-	    break;
+	case EQUAL:
+		fprintf(stderr, "soy igual\n");
+		break;
 
-    case BUILTINCD:
-        // se activará un booleano diciendo que la string es un builtnin
-        fprintf(stderr, "soy cd\n");
-        break;
-    
+	case BUILTINCD:
+		// se activará un booleano diciendo que la string es un builtnin
+		fprintf(stderr, "soy cd\n");
+		break;
+
 	default:
-        fprintf(stderr, "soy palabra\n");
+		fprintf(stderr, "soy palabra\n");
 		break;
 	}
 }
 
 // Mira previamente si se encuentra al final de todo
-int 
-isbg(char *str, int actualpos, int totalpos){
-    
-    //si es true return 1
-    if(actualpos == (totalpos - 1)){
-        if(strcmp(str, "&") == 0){
-            return 1;
-        }
-    }
+int
+isbg(char *str, int actualpos, int totalpos)
+{
 
-    return 0;
+	//si es true return 1
+	if (actualpos == (totalpos - 1)) {
+		if (strcmp(str, "&") == 0) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
+
 // comprueba que hay uno y sólo al principio de la palabra
-int 
-isenv(char *str){
+int
+isenv(char *str)
+{
 
-    int i;
-    int len;
+	int i;
+	int len;
 
-    len = strlen(str); 
+	len = strlen(str);
 
-    // Si primer carácter no es '$' devuelve falso
-    if (str[0] != '$') {
-        return 0;
-    }
+	// Si primer carácter no es '$' devuelve falso
+	if (str[0] != '$') {
+		return 0;
+	}
+	// Verifica si el carácter '$' aparece más de una vez
+	for (i = 1; i < len; i++) {
+		if (str[i] == '$') {
+			return 0;
+		}
+	}
 
-    // Verifica si el carácter '$' aparece más de una vez
-    for (i = 1; i < len; i++) {
-        if (str[i] == '$') {
-            return 0;
-        }
-    }
-    
-    // la cadena comienza con '$' y solo aparece una vez
-    return 1;
+	// la cadena comienza con '$' y solo aparece una vez
+	return 1;
 }
 
 // comprueba que hay uno y sólo en medio de la palabra
-int 
-isequal(char *str){
-    
-    int i;
-    int len;
-    int times;
+int
+isequal(char *str)
+{
 
-    len = strlen(str); 
+	int i;
+	int len;
+	int times;
 
-    // Si primer o último carácter es igual a '=' devuelve falso
-    if ((str[0] == '=') || (str[len -1] == '=')) {
-        return 0;
-    }
+	len = strlen(str);
 
-    times = 0;
-    for(i = 1; i < len - 1; i++){
-
-        if (str[i] == '=') {
-            times++;
-        }
-
-    }
-    // comprueba que entre el segundo y 
-    // penúltimo carácter haya solo un igual
-    if(times == 1){
-        return 1;
-    }
-    return 0;
-}
-
-int 
-gettype(char *str, int actualpos, int totalpos){
-
-    if (strcmp(str, "|") == 0) {
-        return PIPE;
-    }
-    if (strcmp(str, "<") == 0) {
-        return STDINRED;
-    } 
-    if (strcmp(str, ">") == 0) {
-        return STDOUTRED;
-    } 
-    if (isbg(str, actualpos, totalpos)) {
-        return BACKGROUND;
-    }
-
-    if (isenv(str)) {
-        return ENV;
-    }
-
-    if (isequal(str)) {
-        return EQUAL;
-    }
-
-    // ZONA DE BUILT-INS
-    if (strcmp(str, "cd") == 0) {
-        return BUILTINCD;
-    }
-
-    // más adelante incluir los opcionales
-
-    return WORD;
-}
-
-void 
-parse(CheckInput *checkinput){
-
-    int i;
-    int value;
-
-    for (i = 0; i < checkinput->numwords; i++) {
-
-        value = gettype(checkinput->words[i], i, checkinput->numwords);
-
-        settype(checkinput, value);
-        
-    }
-}
-
-void
-freememory(CheckInput *checkinput){
-
-    int i; 
-
-    // liberamos el array de strings 
-    for (i = 0; i < checkinput->numwords; i++) {
-        //fprintf(stderr,"%s\n", checkinput->words[i]);
-        free(checkinput->words[i]);
-    }
-    free(checkinput->words);
-}
-
-void
-tokenize(CheckInput *checkinput, char *line){
-
-    int i; 
-    char *saveptr;
-    char *token;
-
-    checkinput->words = (char **)malloc(sizeof(char *)* checkinput->numwords);
-    if (checkinput->words  == NULL) {
-		perror("Error: dynamic memory cannot be allocated");
+	// Si primer o último carácter es igual a '=' devuelve falso
+	if ((str[0] == '=') || (str[len - 1] == '=')) {
+		return 0;
 	}
 
-    // inicializamos cada elemento del array
-    for (i = 0; i < checkinput->numwords; i++) {
-        checkinput->words[i] = (char *)malloc(sizeof(char) * MaxWord);
-        if (checkinput->words[i]  == NULL) {
-		    perror("Error: dynamic memory cannot be allocated");
-	    }
+	times = 0;
+	for (i = 1; i < len - 1; i++) {
 
-    }
+		if (str[i] == '=') {
+			times++;
+		}
 
-    i = 0;
-    token = strtok_r(line, " \t", &saveptr);
-    // copiar el valor de token en el array dinámico
-    strcpy(checkinput->words[i], token); 
-    
-    i++;
-    while (token != NULL && i < checkinput->numwords) {
-        
-        token = strtok_r(NULL, " \t", &saveptr);
-        strcpy(checkinput->words[i], token); 
-        i++;
-    }
+	}
+	// comprueba que entre el segundo y 
+	// penúltimo carácter haya solo un igual
+	if (times == 1) {
+		return 1;
+	}
+	return 0;
 }
 
 int
-getnumwords(char *line){
+gettype(char *str, int actualpos, int totalpos)
+{
 
-    int i;
-    int numwords;
-    int inword;
-    
-    i = 0;
-    numwords = 0;
-    // es falso
-    inword = 0; 
-    while(line[i] != '\0'){
+	if (strcmp(str, "|") == 0) {
+		return PIPE;
+	}
+	if (strcmp(str, "<") == 0) {
+		return STDINRED;
+	}
+	if (strcmp(str, ">") == 0) {
+		return STDOUTRED;
+	}
+	if (isbg(str, actualpos, totalpos)) {
+		return BACKGROUND;
+	}
 
-        if((line[i] != ' ' && line[i] != '\t') && !inword){
-            numwords++;
-            inword = 1;
-        }
+	if (isenv(str)) {
+		return ENV;
+	}
 
-        if((line[i] == ' ' || line[i] == '\t') && inword){
-            inword = 0;
-        }
-        
-        i++;
-    }
-    return numwords;
+	if (isequal(str)) {
+		return EQUAL;
+	}
+	// ZONA DE BUILT-INS
+	if (strcmp(str, "cd") == 0) {
+		return BUILTINCD;
+	}
+	// más adelante incluir los opcionales
+
+	return WORD;
+}
+
+void
+parse(CheckInput * checkinput)
+{
+
+	int i;
+	int value;
+
+	for (i = 0; i < checkinput->numwords; i++) {
+
+		value = gettype(checkinput->words[i], i, checkinput->numwords);
+
+		settype(checkinput, value);
+
+	}
+}
+
+void
+freememory(CheckInput * checkinput)
+{
+
+	int i;
+
+	// liberamos el array de strings 
+	for (i = 0; i < checkinput->numwords; i++) {
+		//fprintf(stderr,"%s\n", checkinput->words[i]);
+		free(checkinput->words[i]);
+	}
+	free(checkinput->words);
+}
+
+void
+tokenize(CheckInput * checkinput, char *line)
+{
+
+	int i;
+	char *saveptr;
+	char *token;
+
+	checkinput->words =
+	    (char **)malloc(sizeof(char *) * checkinput->numwords);
+	if (checkinput->words == NULL) {
+		perror("Error: dynamic memory cannot be allocated");
+	}
+	// inicializamos cada elemento del array
+	for (i = 0; i < checkinput->numwords; i++) {
+		checkinput->words[i] = (char *)malloc(sizeof(char) * MaxWord);
+		if (checkinput->words[i] == NULL) {
+			perror("Error: dynamic memory cannot be allocated");
+		}
+
+	}
+
+	i = 0;
+	token = strtok_r(line, " \t", &saveptr);
+	// copiar el valor de token en el array dinámico
+	strcpy(checkinput->words[i], token);
+
+	i++;
+	while (token != NULL && i < checkinput->numwords) {
+
+		token = strtok_r(NULL, " \t", &saveptr);
+		strcpy(checkinput->words[i], token);
+		i++;
+	}
+}
+
+int
+getnumwords(char *line)
+{
+
+	int i;
+	int numwords;
+	int inword;
+
+	i = 0;
+	numwords = 0;
+	// es falso
+	inword = 0;
+	while (line[i] != '\0') {
+
+		if ((line[i] != ' ' && line[i] != '\t') && !inword) {
+			numwords++;
+			inword = 1;
+		}
+
+		if ((line[i] == ' ' || line[i] == '\t') && inword) {
+			inword = 0;
+		}
+
+		i++;
+	}
+	return numwords;
 }
--- shell/shell.c
+++ shell/shell.c
@@ -15,56 +15,50 @@ usage(void)
 	exit(EXIT_FAILURE);
 }
 
+int
+main(int argc, char *argv[])
+{
 
-int 
-main(int argc, char *argv[]){
+	char line[MaxLine];
 
-    char line[MaxLine];
-    //int numwords;
-    CheckInput checkinput;
+	//int numwords;
+	CheckInput checkinput;
 
-    argc--;
+	argc--;
 	argv++;
 
-    if (argc != ZeroArgs) {
+	if (argc != ZeroArgs) {
 		usage();
 	}
 
-    printf("Shell made by Julia López Augusto\n");
+	printf("Shell made by Julia López Augusto\n");
 	printf("Write EXIT to leave\n");
 
+	while (1) {
 
-    while (1) {
-
-        if(fgets(line, MaxLine, stdin) == NULL){
-            exit(EXIT_SUCCESS);
-        }
-
-        // elimina el salto de línea al final
+		if (fgets(line, MaxLine, stdin) == NULL) {
+			exit(EXIT_SUCCESS);
+		}
+		// elimina el salto de línea al final
 		line[strcspn(line, "\n")] = 0;
 
-        checkinput.numwords = getnumwords(line);
-        tokenize(&checkinput, line);
-        // una vez tokenizado hay que distinguir cada caso
-        parse(&checkinput);
+		checkinput.numwords = getnumwords(line);
+		tokenize(&checkinput, line);
+		// una vez tokenizado hay que distinguir cada caso
+		parse(&checkinput);
 
+		freememory(&checkinput);
 
+		if (strcmp(line, "EXIT") == 0) {
+			exit(EXIT_SUCCESS);
+		}
 
+		/*if (feof(stdin)) {
+		   // cerrar todo lo necesario y salir
+		   freememory(&checkinput, numwords);
+		   exit(EXIT_SUCCESS);
+		   } */
+	}
 
-
-
-        freememory(&checkinput);
-
-        if(strcmp(line, "EXIT") == 0){
-            exit(EXIT_SUCCESS);
-        }
-
-        /*if (feof(stdin)) {
-            // cerrar todo lo necesario y salir
-            freememory(&checkinput, numwords);
-            exit(EXIT_SUCCESS);
-        }*/
-    }
-
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
