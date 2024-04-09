bcaa95cee8e3c2ffb402cb7fed58be07c927dc9d
Julia <jlopeza2020@alumnos.urjc.es>
2024-04-09 19:43:25 +0200

    added exemple function pointer

--- ejemplos_clase/ejemplos_puntero_fun/ejemplo_puntero_fun.c
+++ ejemplos_clase/ejemplos_puntero_fun/ejemplo_puntero_fun.c
@@ -1,12 +1,14 @@
-void 
-prplus(int a )
+#include <stdio.h>
+
+void prplus(int a)
 {
-    printff(a +1)
+    printf("%d\n", a + 1); // Corregido: Cambiado printff a printf y añadido el formato para el entero.
 }
 
-void (*printsome)(int a );
-
-printsome = prplus;
-printsome(1);
-...
-arreglar 
+int main()
+{
+    void (*printsome)(int); // Corregido: La declaración de printsome debe ser dentro de la función main.
+    printsome = prplus;
+    printsome(3);
+    return 0;
+}
